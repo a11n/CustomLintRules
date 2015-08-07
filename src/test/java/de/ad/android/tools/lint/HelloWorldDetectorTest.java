d6576f651ec059f0cef033019fd976921559b8ba
a11n <andre.diermann@gmail.com>
2015-08-07 11:06:56 +0200

    Updated to latest version of lint-api. Removed lint-junit-rule dependency. Added official lint-tests library. Refactored HelloWorldDetectorTest to work with official test approach. Refactored test-resources file structure.

--- src/test/java/de/ad/android/tools/lint/HelloWorldDetectorTest.java
+++ src/test/java/de/ad/android/tools/lint/HelloWorldDetectorTest.java
@@ -1,42 +1,39 @@
 package de.ad.android.tools.lint;
 
-import com.ad.android.tools.lint.Lint;
-import com.android.tools.lint.Warning;
+import com.android.tools.lint.checks.infrastructure.LintDetectorTest;
+import com.android.tools.lint.detector.api.Detector;
+import com.android.tools.lint.detector.api.Issue;
+import java.io.InputStream;
+import java.util.Arrays;
 import java.util.List;
-import org.junit.Rule;
-import org.junit.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+public class HelloWorldDetectorTest extends LintDetectorTest {
 
-public class HelloWorldDetectorTest {
-  @Rule public Lint lint = new Lint();
-
-  @Test
-  public void shouldFindNoIssues() throws Exception {
-    lint.setFiles("helloWorld/valid/AndroidManifest.xml");
-    lint.setIssues(HelloWorldDetector.ISSUE);
-
-    lint.analyze();
-
-    List<Warning> warnings = lint.getWarnings();
-
-    assertThat(warnings).isEmpty();
+  @Override protected Detector getDetector() {
+    return new HelloWorldDetector();
   }
 
-  @Test
-  public void shouldDetectIssue() throws Exception {
-    lint.setFiles("helloWorld/invalid/AndroidManifest.xml");
-    lint.setIssues(HelloWorldDetector.ISSUE);
+  @Override protected List<Issue> getIssues() {
+    return Arrays.asList(HelloWorldDetector.ISSUE);
+  }
 
-    lint.analyze();
+  @Override protected InputStream getTestResource(String relativePath, boolean expectExists) {
+    String path = relativePath; //$NON-NLS-1$
+    InputStream stream = this.getClass().getClassLoader().getResourceAsStream(path);
+    if (!expectExists && stream == null) {
+      return null;
+    }
+    return stream;
+  }
 
-    List<Warning> warnings = lint.getWarnings();
+  public void testShouldDetectNoWarning() throws Exception {
+    assertEquals("No warnings.", lintFiles("ValidAndroidManifest.xml=>AndroidManifest.xml"));
+  }
 
-    assertThat(warnings).hasSize(1);
-
-    assertThat(warnings.get(0).message).isEqualTo(
-        "Unexpected title \"@string/app_name\". Should be \"Hello world.\".");
-    assertThat(warnings.get(0).file.getName()).isEqualTo("AndroidManifest.xml");
-    assertThat(warnings.get(0).line).isEqualTo(7);
+  public void testShouldDetectWarning() throws Exception {
+    assertEquals("AndroidManifest.xml:8: Information: Unexpected title \"@string/app_name\". Should be \"Hello world.\". [HelloWorld]\n"
+        + "        android:label=\"@string/app_name\"\n"
+        + "        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+        + "0 errors, 1 warnings\n", lintFiles("InvalidAndroidManifest.xml=>AndroidManifest.xml"));
   }
 }
